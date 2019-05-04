7a8909ad44c733fc5682e5f6a23aa85e015a9e9f
kjfanj <kjfanj@gmail.com>
2019-05-03 22:39:15 -0500

    read text file for subreddits

--- kw_reddit/spiders/reddit_spider.py
+++ kw_reddit/spiders/reddit_spider.py
@@ -1,21 +1,27 @@
 import scrapy
+import os
 
 
 class RedditSpider(scrapy.Spider):
     name = "reddit"
 
     def start_requests(self):
+        with open("spiders/subreddits.txt") as f:
+            read_data = f.read()
+            print(read_data)
+        # files = [f for f in os.listdir('.') if os.path.isfile(f)]
+        # for f in files:
+        #     print(f)
         urls = [
-            'https://www.reddit.com/r/pathofexile/',
-            # 'http://quotes.toscrape.com/page/1/',
-            # 'http://quotes.toscrape.com/page/2/',
+            'https://www.reddit.com',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         print("printing response.body")
-        print(response.body)
+        # print(response.body)
+
         # page = response.url.split("/")[-2]
         # filename = 'quotes-%s.html' % page
         # with open(filename, 'wb') as f:
