c30a1965303fdcd24f75cfad498d3a8244855b41
kjfanj <kjfanj@gmail.com>
2019-05-09 23:58:09 -0500

    added more explanation for some methods

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import praw
+# keys for praw
 from settings import CLIENT_SECRET, CLIENT_ID, USER_AGENT
 from datetime import datetime
+# class def
 from SubredditKW import SubredditKW
 
 
 def get_lines(filename):
+    """
+    return a list of each line in the file
+    """
     with open(filename) as f:
         read_data = f.read()
         return read_data.split('\n')
 
 
-def get_keywoards(lines):
+def get_subreddit_kws(lines):
+    """
+    return a list of SubredditKW after parsing 
+    """
     subreddit_kw = []
     for line in lines:
         split_line = line.split(',')
@@ -24,30 +32,30 @@ def get_keywoards(lines):
 
 
 def main():
-    # parse the subreddits file
-    # lines holds each line in 'subreddits.txt' as subreddit, kw1, kw2...
     lines = get_lines('subreddits.txt')
-    # subreddit_kws hold an array of SubredditKW object
-    subreddit_kws = get_keywoards(lines)
+    subreddit_kws = get_subreddit_kws(lines)
+
     for subreddit_kw in subreddit_kws:
         print(subreddit_kw.subreddit)
         print(subreddit_kw.keywords)
 
-    # reddit = praw.Reddit(client_id=CLIENT_ID,
-    #                      client_secret=CLIENT_SECRET,
-    #                      user_agent=USER_AGENT)
+    reddit = praw.Reddit(client_id=CLIENT_ID,
+                         client_secret=CLIENT_SECRET,
+                         user_agent=USER_AGENT)
 
-    # for submission in reddit.subreddit('space').hot(limit=3):
-    #     print("Title:", submission.title)
-    #     print("self text:")
-    #     print("-------------------------------------------------------------")
-    #     print(submission.selftext)
-    #     print("-------------------------------------------------------------")
-    #     print("Upvotes:", submission.score)
-    #     print("By:", submission.author)
-    #     print("Num comment:", submission.num_comments)
-    #     print("Upvote ratio:", submission.upvote_ratio)
-    #     print("")
+    for submission in reddit.subreddit('space').hot(limit=3):
+        print("Title:", submission.title)
+        print("self text:")
+        print("-------------------------------------------------------------")
+        print(submission.selftext)
+        print("-------------------------------------------------------------")
+        print("Upvotes:", submission.score)
+        print("By:", submission.author)
+        print("Num comment:", submission.num_comments)
+        print("Upvote ratio:", submission.upvote_ratio)
+        print("")
+
+    # comment example here
     # print("comments**************************")
     # for comment in submission.comments:
     #     print("----------------------------------")
